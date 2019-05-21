3d3a946eba678ce89544963713c1aeddc0128a63
Beginning0 <39624026+Beginning0@users.noreply.github.com>
2019-05-21 03:44:22 -0400

    https://github.com/BeginningTerroSan/ionic.git

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  constructor(public alertCrtl : AlertController) { }
+  mhw: string;
+  async verSelecion(){
+    let alert = await this.alertCrtl.create({
+      message:this.mhw,
+      buttons:['Cerrar']
+    });
+    alert.present();
+  }
+
+}
